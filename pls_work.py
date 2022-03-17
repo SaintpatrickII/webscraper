8778a8c7ff833b95aca93eadeff37acc7596f06a
SaintpatrickII <92804317+SaintpatrickII@users.noreply.github.com>
2022-03-17 11:33:00 +0000

    Add files via upload

---
+++ pls_work.py
@@ -0,0 +1,107 @@
+from selenium import webdriver
+import time
+from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import uuid
+import json
+import urllib.request
+
+
+class Webscraper:
+    def __init__(self):
+        self.link_list = []
+        self.driver = webdriver.Chrome('/Users/paddy/Downloads/chromedriver')
+        self.driver.get('https://coinmarketcap.com/')
+        self.url = 'https://coinmarketcap.com/'
+        self.next_page_string = '?page='
+        
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cmc-cookie-policy-banner__close"))).click()
+        sleep(10)
+
+
+
+
+    def individual_coin_path(self):
+        
+        sleep(2)
+        coin_container = self.driver.find_element_by_xpath('//table[@class="h7vnx2-2 czTsgW cmc-table  "]')
+        coin_list = coin_container.find_elements_by_xpath('./tbody/tr')
+        return coin_list
+
+
+
+
+    def crypto_properties(self):
+        coin_list = self.individual_coin_path()
+        self.link_list = []
+        sleep(3)
+        i = 1
+        self.driver.execute_script("window.scrollBy(0, 50)")
+        self.driver.execute_script("document.body.style.zoom='50%'")
+        print(len(coin_list))
+        for i in range(len(coin_list)):
+            full_coin_list= [{
+                'uuid' : str(uuid.uuid4()),
+                
+                'Name': coin_list[i].find_element_by_xpath('.//td[3]//a//p').text,
+                'Symbol' : coin_list[i].find_element_by_xpath('.//td[3]/div/a/div/div/div/p').text,
+                'Price' : coin_list[i].find_element_by_xpath('.//td[4]/div/a/span').text,
+                'Volume' :coin_list[i].find_element_by_xpath('.//td[8]/div/a/p').text,
+                'Market_cap' : coin_list[i].find_element_by_xpath('.//td//p/span[2]').text,
+                'Circulating_Supply' : coin_list[i].find_element_by_xpath('.//td[9]//div/div[1]/p').text
+            }]
+            img = coin_list[i].find_element_by_class_name('coin-logo')
+            src = img.get_attribute('src')
+            coin_image = urllib.request.urlretrieve(src, "my_image.png")
+            #i += 1
+            print(full_coin_list)
+            coin_list = self.individual_coin_path()
+            #WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="__next"]/div/div[1]/div[2]/div/div/div[5]/table/tbody/tr')))
+            self.driver.execute_script("window.scrollBy(0, 50)")
+            self.link_list.extend(full_coin_list)
+            self.link_list.extend(coin_image)
+            if i == 100:
+                # self.link_list.extend(full_coin_list)
+                # self.link_list.extend(coin_image)
+                self.save_to_json()
+                return full_coin_list
+            # self.driver.execute_script("window.scrollBy(0, 300)")
+            #
+
+
+
+
+    def scroller(self):
+        self.driver.execute_script("window.scrollBy(0, 300)")
+
+    def save_to_json(self):
+            complete_full_coin_list = self.crypto_properties()
+            crypto_json = json.dumps(complete_full_coin_list)
+            with open('JSON_test.json', encoding='utf-8', mode='w') as file:
+                json.dump(crypto_json, file, ensure_ascii=False, indent=4)
+
+
+
+    def page_iterator(self, no_of_pages):
+        sleep(5)
+        element = self.driver.find_elements_by_xpath("//a[@aria-label='Next page']")
+        page = 1
+        while page <= no_of_pages:
+            self.crypto_properties()
+            self.scroller()
+            self.driver.execute_script("document.body.style.zoom='100%'")
+            next_page_button = element[1]
+            sleep(3)
+            next_page_button.click()
+            page += 1
+            if page == no_of_pages:
+                self.save_to_json()
+        return 
+
+
+
+public_webscraper = Webscraper()
+
+public_webscraper.page_iterator(10)
