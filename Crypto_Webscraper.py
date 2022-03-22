ee6aeb0cbcec500ab5a9593163d48d978ee972d2
SaintpatrickII <92804317+SaintpatrickII@users.noreply.github.com>
2022-03-22 17:31:26 +0000

    Add files via upload

--- Crypto_Webscraper.py
+++ Crypto_Webscraper.py
@@ -15,8 +15,8 @@ import os
 
 class Webscraper:
     def __init__(self):
-        #self.link_list = []
-        self.coin_completed = []
+        self.link_list = []
+        self.coin_image_completed = []
         self.driver = webdriver.Chrome('/Users/paddy/Downloads/chromedriver')
         self.driver.get('https://coinmarketcap.com/')
         self.url = 'https://coinmarketcap.com/'
@@ -25,7 +25,16 @@ class Webscraper:
         WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "div.cmc-cookie-policy-banner__close"))).click()
         sleep(10)
 
-
+    """
+    Initaliser:
+    
+    defines driver local location, creates
+    empty lists to be used in webscraper &
+    defines website to be webscraped, also
+    clicks cookie banner
+    """
+    
+    
     def individual_coin_path(self):
         
         sleep(2)
@@ -33,10 +42,18 @@ class Webscraper:
         coin_list = coin_container.find_elements_by_xpath('./tbody/tr')
         return coin_list
 
+    
+    """
+    Individual_coin_path:
+    
+    defines table on webpage which is
+    to be iterated through to scrape 
+    properties of various coins
+    """
+
 
     def crypto_properties(self):
         coin_list = self.individual_coin_path()
-        self.link_list = []
         self.image_path = '/Users/paddy/Desktop/AiCore/Scraper_Project/Coin_Images'
         sleep(3)
         i = 1
@@ -65,26 +82,44 @@ class Webscraper:
             self.driver.execute_script("window.scrollBy(0, 50)")
             # for coin in self.link_list:
             #     if coin not in self.link_list:
-            #         self.link_list.extend(full_coin_list)
-            self.link_list.extend(full_coin_list)
-            self.coin_completed.extend([coin_image])
+            #         self.link_list.append(full_coin_list)
+            #self.link_list.extend(full_coin_list)
             self.save_to_json()
+            self.coin_image_completed.extend([coin_image])
             if i == 100:
                 #self.save_to_json()
                 return full_coin_list()
 
 
+    """
+    crypto_properties:
+    
+    main bulk of webscraper, this scrapes
+    all items from each coin i.e. images &
+    attributes alongside scrolling the 
+    webscraper to the bottom of each page
+    """
+
+
     def save_to_json(self):
         final_coin_list = []
         for coin in self.link_list:
-            if coin not in self.link_list:
+            if coin not in self.final_link_list:
                 final_coin_list.append(coin)
         complete_full_coin_list = final_coin_list
         crypto_json = json.dumps(complete_full_coin_list,)
-        with open('coins.json', encoding='utf-8', mode='w') as file:
+        with open('coins.json', encoding='utf-8', mode='a') as file:
             json.dump(crypto_json, file, ensure_ascii=False, indent=4)
 
         
+    """
+    save_to_json:
+    
+    responsible for ensuring no duplicate 
+    results saved & all coins saved to
+    easy to read json format
+    """
+
 
     def page_iterator(self, no_of_pages):
         sleep(5)
@@ -102,24 +137,36 @@ class Webscraper:
                 return 
 
 
-    def save_to_s3(self):
-        s3_client = boto3.client('s3')
-        response = s3_client.upload_file('/Users/paddy/Desktop/AiCore/Scraper_Project/Coin_Images', 'patrickcryptobucket', 'coin_images')
+    """
+    page_iterator:
+    
+    responsible for iterating
+    webscraper through first 10
+    pages of website, alongside 
+    saving results once webscraper 
+    finishes
+    
+    input: no_of_pages = int
+    """
 
-    def upload_file(file_name, bucket, object_name=None):
+    # def save_to_s3(self):
+    #     s3_client = boto3.client('s3')
+    #     response = s3_client.upload_file('/Users/paddy/Desktop/AiCore/Scraper_Project/Coin_Images', 'patrickcryptobucket', 'coin_images')
 
-    # If S3 object_name was not specified, use file_name
-        if object_name is None:
-            object_name = os.path.basename('coin_images')
+    # def upload_file(file_name, bucket, object_name=None):
 
-        # Upload the file
-        s3_client = boto3.client('s3')
-        try:
-            response = s3_client.upload_file('/Users/paddy/Desktop/AiCore/Scraper_Project/Coin_Images', 'patrickcryptobucket', 'coin_images')
-        except ClientError as e:
-            logging.error(e)
-            return False
-        return True
+    # # If S3 object_name was not specified, use file_name
+    #     if object_name is None:
+    #         object_name = os.path.basename('coin_images')
+
+    #     # Upload the file
+    #     s3_client = boto3.client('s3')
+    #     try:
+    #         response = s3_client.upload_file('/Users/paddy/Desktop/AiCore/Scraper_Project/Coin_Images', 'patrickcryptobucket', 'coin_images')
+    #     except ClientError as e:
+    #         logging.error(e)
+    #         return False
+    #     return True
 
 
 if __name__ == '__main__':
